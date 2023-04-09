e0b2467c53e810be93124f3dfc389ee912c23638
rajatbhagat <rajat.bhagat@gmail.com>
2023-04-08 20:47:00 -0400

    MOdified the UI and added bootstrap dependencies

--- src/components/customer/CustomerRegistration.js
+++ src/components/customer/CustomerRegistration.js
@@ -1,12 +1,37 @@
-import {Button, TextField} from "@mui/material";
-import {Link} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const CustomerRegistration = () => {
     return (
-        <div className="container d-block">
-            <TextField id="outlined-basic" label="Username" variant="outlined" />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
-            <Link to="/customer/home"><Button variant="contained">Register</Button></Link>
+        <div className="container">
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Email"/>
+
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="First Name"/>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Last Name"/>
+                    <Form.Label>Street</Form.Label>
+                    <Form.Control type="text" placeholder="Street"/>
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="text" placeholder="State"/>
+                    <Form.Label>ZipCode</Form.Label>
+                    <Form.Control type="number" placeholder="ZipCode"/>
+                    <Form.Label>Contact Number</Form.Label>
+                    <Form.Control type="number" placeholder="Contact Number"/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 }

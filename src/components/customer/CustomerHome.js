39c0e22e75f63ce3fc8d733bbb8fcf7fdf2505ed
kanishkasoni <soni.kan@northeastern.edu>
2023-04-19 11:10:20 -0400

    checkout changes

--- src/components/customer/CustomerHome.js
+++ src/components/customer/CustomerHome.js
@@ -1,17 +1,36 @@
 import {useSelector} from "react-redux";
 import MakeReservation from "./MakeReservation";
 import PlaceFoodOrder from "./PlaceFoodOrder";
+import Button from "react-bootstrap/Button";
+import {checkoutCustomer} from "../../services/customer-service";
+import {useNavigate} from "react-router-dom";
 
 const CustomerHome = () => {
 
     const {currentCustomer, reservationDetails} = useSelector(state => state.customerData);
-    console.log(currentCustomer);
-    console.log(reservationDetails);
+    console.log("id", currentCustomer.customerID);
+    console.log("number", reservationDetails.reservationNumber);
+
+    const handleCheckout = async () => {
+        const customerId = currentCustomer.customerID;
+        const resNumber = reservationDetails.reservationNumber;
+        const res = await checkoutCustomer(customerId, resNumber);
+        if(res){
+            alert("Checkout done");
+        }
+
+
+    }
     return (
         <div>
             <h1> Customer Home {currentCustomer.firstName}</h1>
-            <MakeReservation />
-            <PlaceFoodOrder />
+            <Button
+                onClick={() => handleCheckout()}
+                variant="primary">
+                Checkout
+            </Button>
+            <MakeReservation/>
+            <PlaceFoodOrder/>
         </div>
     );
 }

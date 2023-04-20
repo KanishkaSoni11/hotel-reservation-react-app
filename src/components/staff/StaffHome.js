a828e1f40ebaebf0d33dde9b2e456a594a6eb949
rajatbhagat <rajat.bhagat@gmail.com>
2023-04-20 09:06:07 -0400

    Added local storage and logout functionality for staff

--- src/components/staff/StaffHome.js
+++ src/components/staff/StaffHome.js
@@ -1,13 +1,16 @@
 import StaffLogin from "./StaffLogin";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
-import {Button} from "@mui/material";
+import Button from "react-bootstrap/Button";
 import ActiveReservations from "./ActiveReservations";
+import {useEffect} from "react";
+import {getStaffDetailsFromLocalStorageThunk} from "../../services/staff-thunk";
 
 const StaffHome = () =>{
 
     const {currentStaff} = useSelector(state => state.staffData);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleFoodButton = async () => {
         navigate("/staff/food");
@@ -16,25 +19,48 @@ const StaffHome = () =>{
     const handleRoomButton = async () => {
         navigate("/staff/room");
     }
+
+    useEffect(() => {
+        const fetchDataFromLocalStorage = async () => {
+            if (localStorage.getItem("staffDetails")) {
+                const staffDetails = JSON.parse(localStorage.getItem("staffDetails"));
+                const res = await dispatch(getStaffDetailsFromLocalStorageThunk(staffDetails));
+            }
+        }
+        fetchDataFromLocalStorage();
+    }, [])
+
+
     return(
 
         <div className="container p-2 m-2">
         <h1 > Welcome to Staff Portal {currentStaff.firstName}</h1>
-            <Button variant="contained"
+            <Button variant="primary"
                     type="submit"
-                    className="btn btn-dark m-2 p-2 "
+                    className="m-2 p-2 "
                     onClick={handleFoodButton}>
                 Pending Food Orders
 
             </Button>
 
-            <Button variant="contained"
+            <Button variant="primary"
                     type="submit"
-                    className="btn btn-dark m-2 p-2"
+                    className="m-2 p-2"
                     onClick={handleRoomButton}>
                 Pending Room Assignments
 
             </Button>
+            <Button className="justify-content-end m-2 p-2" variant="danger" onClick={() => {
+                if (localStorage.getItem("customerDetails")) {
+                    console.log("Logging out")
+                    localStorage.removeItem("customerDetails");
+                    navigate("/")
+                } else {
+                    console.log("Logging out")
+                    localStorage.removeItem("staffDetails");
+                    navigate("/")
+                }
+            }}>Logout</Button>
             <ActiveReservations/>
             <br></br>
             { console.log("home" ,currentStaff)}
